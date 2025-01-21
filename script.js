bd359802609ac88f5d03a32247c84bc677a48ac1
Hakim-56 <hakimhazlif@gmail.com>
2025-01-22 00:29:17 +0100

    Develop todo

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  function addTask() {
+    const taskText = taskInput.ariaValueMax.trim();
+
+    if (!taskText) alert("Enter a task");
+
+    const taskEl = document.createElement("li");
+    taskEl.textContent = taskText;
+    const removeBtn = document.createElement("button");
+    removeBtn = "Remove";
+    removeBtn.classList.add("remove-btn");
+
+    removeBtn.addEventListener("click", function () {
+      taskEl.remove;
+    });
+
+    taskEl.appendChild(removeBtn);
+    taskList.appendChild(taskEl);
+    taskInput.value = "";
+  }
+
+  addButton.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") addTask();
+  });
+
+  document.addEventListener("DOMContentLoaded", addTask);
+});
